2987b44f4ee7670e97a14f4ca99f2993df4ee8c3
Harsha4517 <anumolusriharsha45@gmail.com>
2025-06-10 21:49:19 +0530

    feat: p_217

---
+++ p_217.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
+
+//leetcode url: https://leetcode.com/problems/contains-duplicate/
+public class p_217 {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 1};
+        Solution solution = new Solution();
+        Boolean result = solution.containsDuplicate(input);
+        System.out.println("The result is: " + result);
+    }
+}
