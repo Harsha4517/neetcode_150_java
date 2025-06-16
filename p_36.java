57ea6d5a4690057384ef84ec1294a8fabcc4478d
Harsha4517 <anumolusriharsha45@gmail.com>
2025-06-16 20:32:27 +0530

    feat: p_36

---
+++ p_36.java
@@ -0,0 +1,70 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+
+class ValidSudokuSolution {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.')
+                    continue;
+
+                String squareKey = (r / 3) + "," + (c / 3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                        cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                        squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
+                    return false;
+                }
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
+            }
+        }
+        return true;
+    }
+}
+
+// logic with using only one hashset for saving space
+class StringSudokuSolution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (!seen.add(board[i][j] + "seen in row" + i) || !seen.add(board[i][j] + "seen in column" + j)
+                            || !seen.add(board[i][j] + "seen in sub matrix" + i / 3 + "-" + j / 3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+// leetcode url: https://leetcode.com/problems/valid-sudoku/
+public class p_36 {
+    public static void main(String[] args) {
+        char[][] board = {
+                { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+        };
+
+        ValidSudokuSolution solution = new ValidSudokuSolution();
+        boolean result = solution.isValidSudoku(board);
+        System.out.println("The result is: " + result);
+    }
+}
